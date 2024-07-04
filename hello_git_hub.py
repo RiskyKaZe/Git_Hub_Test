e35c5868308fa7309e39d524a951df579948ee4e
Rishikesh Ranjan <rishikesh220107@gmail.com>
2024-07-04 22:16:26 +0100

    Successfully Fixed the Issue with Font Styles
    Celebrating the Fixing of the Font Style Issue!

--- hello_git_hub.py
+++ hello_git_hub.py
@@ -1,2 +1,3 @@
 print("Hello GitHub!")
 print("This is my first time trying to use GitHub.")
+print("I am trying to Change the Font Style in VS Code to JetBrains Mono. But, it just doesn't seem to work!")
